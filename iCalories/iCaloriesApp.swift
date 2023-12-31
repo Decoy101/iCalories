d19b24adbf038ea79cf29b158616f6dfb494de84
Decoy101 <00amangpt00@gmail.com>
2023-12-31 15:14:50 +0530

    Initial Commit

---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Aman Gupta on 31/12/23.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
